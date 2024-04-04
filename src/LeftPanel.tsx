8b21fc0f40f21eebd7b949e21cf3344a610dea88
Manuel Duran <manuelduran@Manuels-MacBook-Pro.local>
2024-04-04 09:15:01 -0700

    basic panels complete

--- src/LeftPanel.tsx
+++ src/LeftPanel.tsx
@@ -1,12 +1,30 @@
 import Stack from '@mui/material/Stack';
 import Panel from './components/Panel';
+import Textfield from '@mui/material/TextField';
+
 import theme from './theme';
+import { useState } from 'react';
+import GradientButton from './components/Button';
 const LeftPanel = () => {
+    const [form, setForm] = useState({
+        resume: '',
+        jobListing: '',
+    });
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setForm({
+            ...form,
+            [e.target.name]: e.target.value,
+        });
+    }
+
     return (
         <Panel sx={{
             marginRight: theme.spacing(1),
         }}>
             <Stack spacing={2} >
+                <Textfield onChange={handleChange} name="resume" rows={4} multiline label="Input" variant="outlined" />
+                <Textfield onChange={handleChange} name="jobListing" rows={4} multiline label="Input" variant="outlined" />
 
             </Stack>
         </Panel>
--- src/RightPanel.tsx
+++ src/RightPanel.tsx
@@ -1,13 +1,18 @@
 import Stack from '@mui/material/Stack';
 import Panel from './components/Panel';
 import theme from './theme';
+import { useState } from 'react';
+import { Typography } from '@mui/material';
 
 const RightPanel = () => {
+    const [coverLetter, setCoverLetter] = useState('');
+
     return (
         <Panel sx={{
             marginLeft: theme.spacing(1),
-        }}>            <Stack spacing={2}>
-
+        }}>
+            <Stack spacing={2}>
+                <Typography >{coverLetter}</Typography>
             </Stack>
         </Panel>
     )
